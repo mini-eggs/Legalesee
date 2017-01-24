3bc4d46475561de7143254ece7a05337589b6b3d
Evan <evan@crowdhubapps.com>
2017-01-24 10:18:51 -0800

    another android and ios app

---
+++ client/legalesee/index.ios.js
@@ -0,0 +1,9 @@
+/**
+ * @flow
+ */
+
+import React from 'react'
+import { AppRegistry } from 'react-native'
+import Legalesee from './src/'
+
+AppRegistry.registerComponent('legalesee', () => Legalesee);
---
+++ client/legalesee/src/index.js
@@ -0,0 +1,51 @@
+/**
+ * @flow
+ */
+
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+function multiply(n1: number, n2: number): number {
+  return n1 * n2
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  }
+})
+
+class Legalesee extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text>{multiply(14, 14)}</Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    )
+  }
+}
+
+export default Legalesee
